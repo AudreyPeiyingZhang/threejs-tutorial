e375e428cf732ec13f2c9c966a18fa50b2b06412
AudreyPeiyingZhang <98907440+AudreyPeiyingZhang@users.noreply.github.com>
2022-02-04 18:57:48 +0000

    Finished tutorial 04/02

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -5,7 +5,7 @@ const OverlayWrapper = styled.div`
 	position: absolute;
 	width: 100vw;
 	height: 100vh;
-	background: rgba(0, 255, 0);
+	background: pink;
 	display: ${(props) => (props.show ? 'block' : 'none')};
 `;
 
@@ -23,7 +23,7 @@ const VideoIFrame = styled.iframe`
 	height: 70vh;
 `;
 
-const Title = styled.h1`color: blue;`;
+const Title = styled.h1`color: purple;`;
 
 const CloseOverlay = styled.p`
 	color: blue;
