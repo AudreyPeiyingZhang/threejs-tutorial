12ab30f4cf430fd39c543a108d6de5533a661831
AudreyPeiyingZhang <98907440+AudreyPeiyingZhang@users.noreply.github.com>
2022-02-04 19:04:38 +0000

    Lumi's

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
         },
         {
             to: '/cube',
-            title: 'Cube'
+            title: 'Lumi'
         },
         // {
         //     to: '/portfolio',
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
   return (
     <Layout>
         <HomeWrapper>
-            <h1> Thomas' favourite things </h1>
+            <h1> Audrey's world </h1>
             <Bio>
             Adipisicing four dollar toast labore lomo pok pok, in craft beer kogi sriracha vice gentrify. Ut laborum voluptate nulla tempor ea consectetur before they sold out bushwick etsy hammock vegan blue bottle.
             </Bio>
